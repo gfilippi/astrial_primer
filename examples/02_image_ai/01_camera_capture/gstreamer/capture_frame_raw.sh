1f6879512ba6ed83b6b3a2f2b8ea49215077e7f0
Gianluca Filippini <gianluca.filippini@ebv.com>
2024-12-05 07:46:34 +0100

    fix capture script for camera raw

--- examples/02_image_ai/01_camera_capture/gstreamer/capture_frame_raw.sh
+++ examples/02_image_ai/01_camera_capture/gstreamer/capture_frame_raw.sh
@@ -3,8 +3,8 @@
 ######################################################################
 
 # multi-frames: capture few frames for AWB on Astrial
-gst-launch-1.0 v4l2src device=/dev/video3 num-buffers=3 ! 'video/x-raw,width=1920,height=1080,format=YUY2,framerate=30/1' ! multifilesink index=0 location=frame%04d.jpg sync=true async=false
+gst-launch-1.0 v4l2src device=/dev/video3 num-buffers=3 ! 'video/x-raw,width=1920,height=1080,format=YUY2,framerate=30/1' ! multifilesink index=0 location=frame%04d.raw sync=true async=false
 
 # only one frame
-# gst-launch-1.0 v4l2src device=/dev/video2 num-buffers=1 ! 'video/x-raw,width=1920,height=1080,format=YUY2' ! filesink location=image.jpg
+# gst-launch-1.0 v4l2src device=/dev/video2 num-buffers=1 ! 'video/x-raw,width=1920,height=1080,format=YUY2' ! filesink location=image.raw
 
